5539f356cda4812a53e7e5b51e5744043d3d53a0
juanpabarriosr <juanpabarriosr@gmail.com>
2020-05-31 00:14:55 -0500

    Se implementa SignUpStepDefs - Given - se agrega WebDriver

--- src/test/java/com/mesaj/app/stepdefinitions/SignUpStepDefs.java
+++ src/test/java/com/mesaj/app/stepdefinitions/SignUpStepDefs.java
@@ -1,14 +1,26 @@
 package com.mesaj.app.stepdefinitions;
 
+import com.mesaj.app.pageobjects.SignUpPageObject;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SignUpStepDefs {
 
     @Given("^Pepito wants to have an account$")
     public void pepito_wants_to_have_an_account() {
 
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/windows/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        SignUpPageObject signUpPageObject = new SignUpPageObject(driver);
+        signUpPageObject.go("http://demo.automationtesting.in/Register.html");
+        signUpPageObject.writeFirstname("Pepito");
+        signUpPageObject.writeLastName("Perez");
+        signUpPageObject.clickOnSubmit();
     }
 
     @When("^he sends required information to get the account$")
