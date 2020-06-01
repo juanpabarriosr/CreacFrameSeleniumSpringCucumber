6371f4464272b5c0365dca584ddf088cc7b7b4ae
juanpabarriosr <juanpabarriosr@gmail.com>
2020-06-01 01:24:30 -0500

    Se agrega anotación @Getter a clase SignUpPageObject

--- src/test/java/com/mesaj/app/pageobjects/SignUpPageObject.java
+++ src/test/java/com/mesaj/app/pageobjects/SignUpPageObject.java
@@ -1,10 +1,12 @@
 package com.mesaj.app.pageobjects;
 
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+@Getter
 public class SignUpPageObject {
 
     public SignUpPageObject(WebDriver driver){
